b8b57fdb13dbcfffda295112365e73ab877970df
Nitishrai1 <nitish.12208500@lpu.in>
2024-09-21 19:14:00 +0530

    code

--- Week6/Keys/src/App.jsx
+++ Week6/Keys/src/App.jsx
@@ -3,9 +3,37 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+
+function Navbar(){
+  return <div >
+   
+      
+     <ul style={{display:"flex", justifyContent:"space-around", alignItems:"center"  }}>
+      <li>Home</li>
+      <li>About</li>
+      <li>service</li>
+      <li>Contact</li>
+     </ul>
+    </div>
+
+
+}
+
+function CardWrapper({children }){
+  // yaha par main logic lagega
+
+  return (
+    <div style={{backgroundColor:"lightgray" , border:2, padding:5}}>
+      {children }
+      </div>
+  )
+}
 function App() {
   
   return <>
+  <CardWrapper>
+    <Navbar/>
+  </CardWrapper>
 
   </>
 }
