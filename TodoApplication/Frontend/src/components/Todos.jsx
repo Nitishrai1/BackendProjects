4174389714a6063871108878ac9293dcad2e4cf0
Nitishrai1 <nitish.12208500@lpu.in>
2024-09-25 16:48:57 +0530

    update

--- TodoApplication/Frontend/src/components/Todos.jsx
+++ TodoApplication/Frontend/src/components/Todos.jsx
@@ -14,9 +14,8 @@ todos=[
 */
 
 // {todos}  this is called destructuring we can also use props and to extract the tods use const todos= props.todos
-import { useState } from "react";
-export function Todos( {todos,setTodos} ) {
 
+export function Todos( {todos} ) {
   const updatetodo=async (id) => {
     try{
      const token=localStorage.getItem('Token');
@@ -32,9 +31,6 @@ export function Todos( {todos,setTodos} ) {
      const data=await response.json();
      if (response.ok) {
       alert(`Result is ${data.msg}`);
-      setTodos(todos.map((todo)=>{
-        todo._id===id?{...todo, completed:true}:todo
-      })) 
     } else {
       alert(`Error: ${data.msg || "Could not update todo"}`);
     }
@@ -49,7 +45,7 @@ export function Todos( {todos,setTodos} ) {
       {todos.map(function (todo) {
         return (
           // eslint-disable-next-line react/jsx-key
-          <div key={todo._id}>
+          <div>
             <h1>{todo.title}</h1>
             <h1>{todo.description}</h1>
             <button
@@ -60,19 +56,17 @@ export function Todos( {todos,setTodos} ) {
             >
               {todo.completed == true ? "Completed" : "Not completed"}
             </button>
-            {!todo.completed && (
-              <button
-                style={{
-                  padding: 10,
-                  margin: 10,
-                }}
-                onClick={() => {
-                  updatetodo(todo._id);
-                }}
-              >
-                Mark as completed
-              </button>
-            )}
+            <button
+              style={{
+                padding: 10,
+                margin: 10,
+              }}
+              onClick={()=>{
+                updatetodo(todo._id)
+              }}
+            >
+              Mark as completed
+            </button>
           </div>
         );
       })}
