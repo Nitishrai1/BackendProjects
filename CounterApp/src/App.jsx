f61dc84af53e82333260e53a0990231e90ad34c5
Nitishrai1 <nitish.12208500@lpu.in>
2024-10-04 18:48:54 +0530

    code

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import "./App.css";
-import {  useRecoilState, useRecoilValue,RecoilRoot } from "recoil";
+import {  useRecoilState, useRecoilValue,RecoilRoot, useSetRecoilState } from "recoil";
 import { countAtom } from "./store/atom/count";
 
 function App() {
@@ -28,16 +28,19 @@ function CounterRender() {
   return (
     <div>
       <b>Count:{count}</b>
+      <EvencountRenderer />
     </div>
   );
 }
 function Button() {
-  const [count,setCount]=useRecoilState(countAtom);
+  // const [count,setCount]=useRecoilState(countAtom); //we can optimize the code by using the useSetRecoilState() this will not re render the button which need not to be rendered again and again
+  const setCount=useSetRecoilState(countAtom);
+  console.log("Button re-rendered");
   return (
     <div>
       <button
         onClick={() => {
-          setCount(count + 1);
+          setCount(count=>count+1);
         }}
       >
         Incremnet
@@ -45,13 +48,19 @@ function Button() {
       <br />
       <button
         onClick={() => {
-          setCount(count - 1);
+          setCount(count=>count-1);
         }}
       >
         Decrement
       </button>
     </div>
   );
+}
+function EvencountRenderer(){
+  const count=useRecoilValue(countAtom);
+  return <div>
+   { (count%2==0)?"It is even":null}
+  </div>
 }
 
 export default App;
