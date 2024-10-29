ea38bf80e12b9f64a371a7b6e725107156d3f040
Nitishrai1 <nitish.12208500@lpu.in>
2024-10-29 23:30:49 +0530

    update

---
+++ Paytm/backend/db/index.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/paytmApp")
+.then(()=>console.log(`mongodb connected succesfully`))
+.catch((err)=>console.log(`error in connecting to database`))
+
+
+const userSchemaf
