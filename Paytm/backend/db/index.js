8e7daee4d00368991b5a4eedf11d64ad3a4773ed
Nitishrai1 <nitish.12208500@lpu.in>
2024-11-03 16:38:13 +0530

    update

--- Paytm/backend/db/index.js
+++ Paytm/backend/db/index.js
@@ -38,3 +38,4 @@ const userSchema=new mongoose.Schema({
 })
 
 const User=mongoose.model('User',userSchema)
+module.exports=User;
--- Paytm/backend/routes/user.js
+++ Paytm/backend/routes/user.js
@@ -1,5 +1,5 @@
 const { Router } = require("express")
-
+const User=require("../db/index")
 
 
 const router=Router();
@@ -8,7 +8,29 @@ const router=Router();
 router.get();
 
 
-router.post("/signup",userauth,(req,res)=>{
+router.post("/signup",async(req,res)=>{
+    const {username,password,firstname,lastname}=req.body;
+    try{
+        const user=await User.findOne({
+            username:username
+        })
+        if(user){
+            res.status(404).json("User already exist");
+            return;
+        }
+        const newuser=await User.create({
+            username:username,
+            password:password,
+            firstname:firstname,
+            lastname:lastname
+        })
+        newuser.save();
+        return res.status(200).json({msg:"User created successfully"});
+
+    }catch(err){
+
+    }
+
 
 })
 
