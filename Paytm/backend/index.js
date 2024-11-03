fc79f699c28455e67c409596f01b0f3b13999242
Nitishrai1 <nitish.12208500@lpu.in>
2024-11-03 16:30:28 +0530

    checkpoint

--- Paytm/backend/index.js
+++ Paytm/backend/index.js
@@ -1,7 +1,11 @@
 const express=require("express");
-
+const cors=require('cors');
 const app=express();
+require('dotenv').config();
 
+app.use(cors());
+app.use(express.json());
+const JWT_KEY=process.env.JWT_KEY
 const user=require("./routes/user")
 const transaction=require("./routes/transaction")
 
