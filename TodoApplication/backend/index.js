e18965c442eab8ce5c29bb7203cdeedbcee4ba07
Nitishrai1 <nitish.12208500@lpu.in>
2024-11-10 16:34:18 +0530

    updated

--- TodoApplication/backend/index.js
+++ TodoApplication/backend/index.js
@@ -12,26 +12,26 @@ const Notification=require('./db');
 const cors=require('cors');  //cors is liye use hota hai taki jab bhi hamara backend or frontend alag alag locala host par chal raha hota hai to hamara browser allow nahi karta hai dusare local host wale frontend ko dusrare local host ke backend ho hit kare ki permision nahi deta hai cors help karta hai taki wo waysa kar sake with  the help off orogin se kar ke or agar hamko sabko allow karna haio tab empty rakh dena
 app.use(cors())
 
-const server = http.createServer(app);
-const io = new Server(server);
+// const server = http.createServer(app);
+// const io = new Server(server);
 
 // ab yaha par server ka connection establish kar lete hai niche wla code is liye hai taki agar koi new notifiucatin ate hai sendNotification event nam ko to usko catch karke new notifaction bana kar save kar lene ke liye hai 
-io.on('connection',(socket)=>{
-    console.log('Developer connected');
-    socket.on('sendNotification', async ({ developerId, clientEmail, projectDetails }) => {
-        try {
-            const notification = new Notification({ developerId, clientEmail, projectDetails });
-            await notification.save();
-            io.emit(`notification-${developerId}`, notification); 
-        } catch (error) {
-            console.error("Error saving notification:", error);
-        }
-    });
+// io.on('connection',(socket)=>{
+//     console.log('Developer connected');
+//     socket.on('sendNotification', async ({ developerId, clientEmail, projectDetails }) => {
+//         try {
+//             const notification = new Notification({ developerId, clientEmail, projectDetails });
+//             await notification.save();
+//             io.emit(`notification-${developerId}`, notification); 
+//         } catch (error) {
+//             console.error("Error saving notification:", error);
+//         }
+//     });
     
-    socket.on('disconnect',()=>{
-        console.log('Developer disconnected');
-    });
-})
+//     socket.on('disconnect',()=>{
+//         console.log('Developer disconnected');
+//     });
+// })
 
 
 // midle wire
