ae5331be593f1f1200bf9141b449c24ab9da566d
Nitishrai1 <nitish.12208500@lpu.in>
2024-11-27 23:22:20 +0530

    up

--- PrismaORMDatabses/week-13-sql-master/week-13-sql-master/src/insert-data.ts
+++ PrismaORMDatabses/week-13-sql-master/week-13-sql-master/src/insert-data.ts
@@ -3,7 +3,7 @@ import { getClient } from "./utils";
 async function createEntries() {
     const client = await getClient();
     const insertUserText = 'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id';
-    const userValues = ['john.do11e@gmail2.com', 'hashed_password_here'];
+    const userValues = ['nitish.do11e@gmail2.com', 'hashed_password_here'];
 
     let response = await client.query(insertUserText, userValues);
     const insertTodoText = 'INSERT INTO todos (title, description, user_id, done) VALUES ($1, $2, $3, $4) RETURNING id';
--- PrismaORMDatabses/week-13-sql-master/week-13-sql-master/src/joins/advance-1.ts
+++ PrismaORMDatabses/week-13-sql-master/week-13-sql-master/src/joins/advance-1.ts
@@ -1,15 +1,15 @@
 import { getClient } from "../utils";
 
-// Get all todos for a give user
+// Get all todos for a given user
 // This needs to ensure that every user comes atleast once
 async function getUserAndTodosWithJoin(userId: number) {
     const client = await getClient();
 
     const joinQuery = `
-        SELECT users.*, todos.title, todos.description, todos.done
-        FROM users
-        LEFT JOIN todos ON users.id = todos.user_id
-        WHERE users.id = $1;
+            SELECT users.*, todos.title, todos.description, todos.done
+            FROM users
+            LEFT JOIN todos ON users.id = todos.user_id
+            WHERE users.id = $1;
     `;
 
     const res = await client.query(joinQuery, [userId]);
